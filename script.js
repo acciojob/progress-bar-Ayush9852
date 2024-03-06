15492076c57bc09abd98d4df650c07c8cfaf7fec
Ayush9852 <71625438+Ayush9852@users.noreply.github.com>
2024-03-06 15:38:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
-//your JS code here. If required.
- //DOM Elements
+// DOM Elements
 const circles = document.querySelectorAll(".circle"),
   progressBar = document.querySelector(".indicator"),
-  buttons = document.querySelectorAll("button");
+  prevButton = document.getElementById("prev"),
+  nextButton = document.getElementById("next");
 
 let currentStep = 1;
 
-// function that updates the current step and updates the DOM
+// Function to update the current step and update the DOM
 const updateSteps = (e) => {
-  // update current step based on the button clicked
+  // Update current step based on the button clicked
   currentStep = e.target.id === "next" ? ++currentStep : --currentStep;
 
-  // loop through all circles and add/remove "active" class based on their index and current step
+  // Loop through all circles and add/remove "active" class based on their index and current step
   circles.forEach((circle, index) => {
     circle.classList[`${index < currentStep ? "add" : "remove"}`]("active");
   });
 
-  // update progress bar width based on current step
+  // Update progress bar width based on current step
   progressBar.style.width = `${((currentStep - 1) / (circles.length - 1)) * 100}%`;
 
-  // check if current step is last step or first step and disable corresponding buttons
+  // Check if current step is last step or first step and disable corresponding buttons
   if (currentStep === circles.length) {
-    buttons[1].disabled = true;
+    nextButton.disabled = true;
   } else if (currentStep === 1) {
-    buttons[0].disabled = true;
+    prevButton.disabled = true;
   } else {
-    buttons.forEach((button) => (button.disabled = false));
+    prevButton.disabled = false;
+    nextButton.disabled = false;
   }
 };
 
-// add click event listeners to all buttons
-buttons.forEach((button) => {
-  button.addEventListener("click", updateSteps);
+// Check initial state of buttons and circles
+document.addEventListener("DOMContentLoaded", () => {
+  if (currentStep === 1) {
+    prevButton.disabled = true;
+  }
 });
+
+// Add click event listeners to previous and next buttons
+prevButton.addEventListener("click", updateSteps);
+nextButton.addEventListener("click", updateSteps);
